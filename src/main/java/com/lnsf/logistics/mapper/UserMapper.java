add1bde17aad7e36ea8c18279d9550c7382466ea
zndhe <1575908970@qq.com>
2019-05-15 17:47:21 +0800

    test

--- src/main/java/com/lnsf/logistics/mapper/UserMapper.java
+++ src/main/java/com/lnsf/logistics/mapper/UserMapper.java
@@ -8,7 +8,8 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM user")
+
+    @Select("")
     List<User> selectAll();
 
     @Select("SELECT * FROM user WHERE priority = #{priority}")
